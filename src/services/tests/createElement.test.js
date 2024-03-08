5deb6276314a5a74f2d3c8d4bd29bfd66b505473
Владислав Филатов <filatov@reg.ru>
2024-03-08 16:30:19 +0800

    Пофиксил проблемы связанные с запуском тестов

--- src/services/tests/createElement.test.js
+++ src/services/tests/createElement.test.js
@@ -1,8 +1,15 @@
 import { createElement } from '../createElement';
 
-test(
-  'Проверяем создание эллемента',
-  () => {
-    expect(createElement({ tag: 'div', class: 'input-box' })).not.toBe(null);
-  },
-);
+describe('метод createElement', () => {
+  it('должен создавать DOM элемент с указанными параметрами', () => {
+    // First A - Arrange (подготовка)
+    const expectedElement = { tag: 'div', class: 'input-box' };
+
+    // Second A - Act (действие)
+    const element = createElement(expectedElement);
+
+    // Third A - Assert (результат)
+    expect(element.tagName).toBe('DIV');
+    expect(element.classList.contains(expectedElement.class)).toBe(true);
+  });
+});
