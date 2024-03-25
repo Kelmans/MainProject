82159e2e8bd1403304680b3602090a2a9faa97d9
Vladislav <32076727+Kelmans@users.noreply.github.com>
2024-03-25 20:27:58 +0500

    Фикс импортов

--- src/main.js
+++ src/main.js
@@ -3,7 +3,20 @@ import auth from './components/auth/auth';
 
 const app = document.querySelector('#app');
 
-const mongo = require('mongodb').MongoClient;
-
+// const mongo = require('mongodb').MongoClient;
 app.append(Header);
 app.append(auth);
+
+async function postData(url = '') {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return await response.json();
+}
+
+postData('http://localhost:3001/login').then((data) => {
+  console.log(data, '!!');
+});
