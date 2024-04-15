5634e2a1e7b6607ae5b1046f3fac86013be44306
Vladislav <32076727+Kelmans@users.noreply.github.com>
2024-04-15 20:00:34 +0500

    верстка

--- src/components/core-info/core-info.js
+++ src/components/core-info/core-info.js
@@ -14,7 +14,35 @@ a2.innerText = 'Тарология';
 const a3 = createElement({ tag: 'a', class: 'courses-item' });
 a3.innerText = 'Программирование';
 
+const consultation = createElement({ tag: 'div', class: 'consultation' });
+consultation.innerText = 'Тут будет окно с инфой для связи';
+const consultationTitle = createElement({ tag: 'div', class: '' });
+const nameInput = createElement({ tag: 'input', class: '' });
+const emailInput = createElement({ tag: 'input', class: '' });
+const phoneInput = createElement({ tag: 'input', class: '' });
+const button = createElement({ tag: 'button', class: '' });
 
+const briefInformation = createElement({ tag: 'div', class: 'briefInformation' });
+const briefInformationVideo = createElement({ tag: 'div', class: '' });
+briefInformationVideo.innerText = 'Сюда добавляю видео с презентацией';
+const briefInformationText = createElement({ tag: 'div', class: '' });
+const briefInformation1 = createElement({ tag: 'div', class: '' });
+briefInformation1.innerText = 'Индивидуальный путь обучения';
+const briefInformation2 = createElement({ tag: 'div', class: '' });
+briefInformation2.innerText = 'Интерактивные онлайн-уроки с экспертами';
+const briefInformation3 = createElement({ tag: 'div', class: '' });
+briefInformation3.innerText = 'Помощь с познанием мира';
+const briefInformation4 = createElement({ tag: 'div', class: '' });
+briefInformation4.innerText = 'Готовое портфолио по итогам обучения';
+
+briefInformationText.append(
+  briefInformation1,
+  briefInformation2,
+  briefInformation3,
+  briefInformation4,
+);
+briefInformation.append(briefInformationVideo, briefInformationText);
+consultation.append(consultationTitle, nameInput, emailInput, phoneInput, button);
 courses.append(a1, a2, a3);
-coreInfo.append(coreInfoTitle, courses);
+coreInfo.append(coreInfoTitle, courses, consultation, briefInformation);
 export default coreInfo;
